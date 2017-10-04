360c02731a9e88300cd6d17b161ad627922e611f
Nguyen Pham Anh Nguyen <anhnguyen9a7@gmail.com>
2017-10-05 00:53:53 +0700

    Update Header Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Header } from './Header Component';
+import { Header, ControlledHeader } from './Header Component';
 
 class App extends Component {
     render() {
         return (
-            <Header></Header>
+            <ControlledHeader />
         );
     }
 }
--- src/Header Component/Header.jsx
+++ src/Header Component/Header.jsx
@@ -3,7 +3,11 @@ import { Navbar, Nav, NavItem, Button } from 'react-bootstrap';
 import './Header.css';
 import logo from './pics/logo.png';
 
-const Header = () => {
+const Header = ({
+    rw,
+    wr
+}) => {
+    const bg = 'white', txt = 'red';
     return (
         <Navbar fixedTop>
             <Nav>
@@ -14,20 +18,77 @@ const Header = () => {
                 </NavItem>
             </Nav>
             <Nav>
-                <NavItem>
-                    <Button className='button' href='#'>Button1</Button>
+                <NavItem className='button-box'>
+                    <Button
+                        id='button1'
+                        href='#'
+                        onMouseEnter={() => rw('button1')}
+                        onMouseLeave={() => wr('button1')}
+                        onMouseDown={() => rw('button1', 1)}
+                        onMouseUp={() => rw('button1')}
+                        style={{ backgroundColor: bg, color: txt, borderColor: 'white' }}
+                    >
+                        Button1
+                    </Button>
                 </NavItem>
-                <NavItem>
-                    <Button className='button' href='#'>Button2</Button>
+                <NavItem className='button-box'>
+                    <Button
+                        id='button2'
+                        href='#'
+                        onMouseEnter={() => rw('button2')}
+                        onMouseLeave={() => wr('button2')}
+                        onMouseDown={() => rw('button2', 1)}
+                        onMouseUp={() => rw('button2')}
+                        style={{ backgroundColor: bg, color: txt, borderColor: 'white' }}
+                    >
+                        Button2
+                    </Button>
                 </NavItem>
             </Nav>
             <Nav pullRight>
                 <NavItem>
-                    <Button className='button' href='#'>Login</Button>
+                    <Button
+                        id='login'
+                        href='#'
+                        onMouseEnter={() => rw('login')}
+                        onMouseLeave={() => wr('login')}
+                        onMouseDown={() => rw('login', 1)}
+                        onMouseUp={() => rw('login')}
+                        style={{ backgroundColor: bg, color: txt, borderColor: 'white' }}
+                    >
+                        Login
+                    </Button>
                 </NavItem>
             </Nav>
         </Navbar>
     );
 };
 
-export { Header };
+const control = WrappedComponent =>
+    class extends Component {
+        rw = (id, border = null) => {
+            document.getElementById(id).style.backgroundColor = 'red';
+            document.getElementById(id).style.color = 'white';
+            border
+                ? document.getElementById(id).style.borderColor = 'grey'
+                : document.getElementById(id).style.borderColor = 'white';
+        }
+
+        wr = id => {
+            document.getElementById(id).style.backgroundColor = 'white';
+            document.getElementById(id).style.color = 'red';
+        }
+
+        render() {
+            return (
+                <WrappedComponent
+                    rw={this.rw}
+                    wr={this.wr}
+                />
+            )
+        }
+    }
+
+const ControlledHeader = control(Header);
+
+export { Header, ControlledHeader };
