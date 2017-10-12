ca6e626207b535d670b3244c748dae24f943bd84
Nguyen Pham Anh Nguyen <anhnguyen9a7@gmail.com>
2017-10-12 09:47:47 +0700

    Update Slider to merge

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Slider } from './Slider Component';
+import { ControlledHeader } from './Header Component';
 
 class App extends Component {
     render() {
-        return (
-            <Slider></Slider>
-        );
+        return (null);
     }
 }
 
