1df06a3c303dcf73064bc1877db48a27843113c3
Nguyen Pham Anh Nguyen <anhnguyen9a7@gmail.com>
2017-10-12 10:10:26 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ import { Main } from './Main.jsx';
 import { Slider } from './Slider Component';
 import { ControlledHeader } from './Header Component';
 
-class App extends Component {
+import logo from './logo.svg';
+
+export default class App extends Component {
     render() {
         return (
             <div>
@@ -19,21 +21,21 @@ class App extends Component {
                         <h2>Welcome to React</h2>
                     </div>
                     <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
-                    </p>   
+                        To get started, edit <code>src/App.js</code> and save to reload.
+                    </p>
                     <ul>
                         <li><a href="/searchbox">Search Box</a></li>
                         <li><a href="/searchresult">Search Result</a></li>
                         <li><a href="/comment">Comment</a></li>
                         <li><a href="/pdf">PDF</a></li>
                     </ul>
-                    <br/>      
+                    <br />
                 </div>
 
-                <div style={{marginLeft: '100px'}}>
-                    <Main/>
+                <div style={{ marginLeft: '100px' }}>
+                    <Main />
                 </div>
             </div>
         );
-
-export default App;
+    }
+}
