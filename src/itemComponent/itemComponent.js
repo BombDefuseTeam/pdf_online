1bb2fb8bf68a3ba5068ecea197a03a95c44f83c8
nkhanhcong@gmail.com <1410407@hcmut.edu.vn>
2017-10-12 16:44:35 +0700

    item component

---
+++ src/itemComponent/itemComponent.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import { Image, Button, Icon, Segment, Card, Rating, Grid, Popup } from 'semantic-ui-react';
+import './itemComponent.css';
+
+const DumpItemCardComponent = ({ onMouseLeave, onMouseEnter }) => {
+    const CardItem = (
+
+        <Card style={{ 'text-decoration': 'none' }} link >
+            <Image src={require('../images/unnamed.jpg')} />
+            <Card.Content>
+                <Card.Header>
+                    Songoku
+                            </Card.Header>
+                <Card.Meta>
+                    Join in 2016
+                            </Card.Meta>
+                <Card.Description>
+                    That is incredible commic
+                            </Card.Description>
+
+            </Card.Content>
+            <Card.Content>
+                <Rating icon='star' defaultRating={3} maxRating={4} />
+
+            </Card.Content>
+        </Card>
+
+    )
+    return (
+        <div onMouseLeave={onMouseLeave} onMouseEnter={onMouseEnter}>
+
+            <Segment basic  >
+                <Popup trigger={CardItem} flowing hoverable position='right center'>
+                    <Popup.Header>Test</Popup.Header>
+                    <Popup.Content>
+                        helelle
+                    </Popup.Content>
+                </Popup>
+
+            </Segment >
+        </div >
+    );
+
+};
+
+const controllerItem = WrapperComponent =>
+    class extends Component {
+
+        handleMouseLeave = () => {
+
+        }
+        handleMouseEnter = () => {
+
+        }
+        render() {
+            return <WrapperComponent onMouseLeave={this.handleMouseLeave} onMouseEnter={this.handleMouseEnter} />;
+        }
+    };
+
+
+const SmartItemCardComponent = controllerItem(DumpItemCardComponent);
+export { SmartItemCardComponent };
+
