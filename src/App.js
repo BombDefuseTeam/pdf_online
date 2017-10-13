2ab7d081788e77fb687e338c2881e2f2bd336e28
Nguyen Pham Anh Nguyen <anhnguyen9a7@gmail.com>
2017-10-13 12:04:54 +0700

    Deleted some package-lock

--- src/App.js
+++ src/App.js
@@ -18,22 +18,7 @@ export default class App extends Component {
     render() {
         return (
             <div>
-                <div className="App">
-                    <div className="App-header">
-                        <img src={logo} className="App-logo" alt="logo" />
-                        <h2>Welcome to React</h2>
-                    </div>
-                    <p className="App-intro">
-                        To get started, edit <code>src/App.js</code> and save to reload.
-                    </p>
-                    <ul>
-                        <li><a href="/searchbox">Search Box</a></li>
-                        <li><a href="/searchresult">Search Result</a></li>
-                        <li><a href="/comment">Comment</a></li>
-                        <li><a href="/pdf">PDF</a></li>
-                    </ul>
-                    <br />
-                </div>
+                <ControlledHeader />
 
                 <div style={{ marginLeft: '100px' }}>
                     <Main />
