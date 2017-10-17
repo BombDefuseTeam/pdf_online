afa4971a4416a371d3e27da47c24e9b668872f94
Nguyen Pham Anh Nguyen <anhnguyen9a7@gmail.com>
2017-10-18 00:07:02 +0700

    Added Info Page

--- src/App.js
+++ src/App.js
@@ -14,25 +14,28 @@ import { SmartListItemComponent } from './Components/listItemComponent/listItemC
 import { Button } from 'react-bootstrap';
 import logo from './logo.svg';
 
+import { Info } from './Pages/Info';
+
 export default class App extends Component {
     render() {
         return (
             <div >
-                <div className='Header' >
+                <Info />
+                {/* <div className='Header' >
                     <ControlledHeader />
                 </div>
                 <div>
                     <Slider />
                 </div>
-                <div className="App">
-                    {/* <ul>
+                <div className="App"> */}
+                {/* <ul>
                         <li><a href="/searchbox">Search Box</a></li>
                         <li><a href="/searchresult">Search Result</a></li>
                         <li><a href="/comment">Comment</a></li>
                         <li><a href="/pdf">PDF</a></li>
                     </ul>
                     <br /> */}
-                    <div className='listItem'>
+                {/* <div className='listItem'>
                         <SmartListItemComponent category={'Top rate'} />
                         <SmartListItemComponent category={'Top rate1'} />
                         <SmartListItemComponent category={'Top rate2'} />
@@ -43,7 +46,7 @@ export default class App extends Component {
 
                 <div style={{ position: 'relative', 'margin-top': '60px' }} >
                     <Footer />
-                </div>
+                </div> */}
             </div>
         );
     }
---
+++ src/Pages/Info/Info.jsx
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import { Breadcrumb, Image, PageHeader, ListGroup, ListGroupItem, ButtonToolbar, Button } from 'react-bootstrap';
+import './Info.css';
+
+import { Comment } from '../../Components/Comment';
+import { ControlledHeader } from '../../Components/Header Component';
+import { Footer } from '../../Components/Footer/Footer';
+
+import image from './book1.jpg';
+
+const Info = () => {
+    return (
+        <div>
+            <div><ControlledHeader /></div>
+            <div id="info-content">
+                <div id="item-path">
+                    <Breadcrumb>
+                        <Breadcrumb.Item href="#">Home</Breadcrumb.Item>
+                        <Breadcrumb.Item href="#">{item.category}</Breadcrumb.Item>
+                        <Breadcrumb.Item active>{item.name}</Breadcrumb.Item>
+                    </Breadcrumb>
+                </div>
+                <div id="main-content">
+                    <div id="item-image-box">
+                        <Image id='item-image' src={item.image} responsive thumbnail />
+                    </div>
+                    <div id="item-info-box">
+                        <PageHeader>{item.name}</PageHeader>
+                        <ListGroup>
+                            <ListGroupItem header="Authors">{item.author}</ListGroupItem>
+                            <ListGroupItem header="Year">{item.year}</ListGroupItem>
+                            <ListGroupItem header="Genres">{item.genre}</ListGroupItem>
+                            <ListGroupItem header="Language">{item.lang}</ListGroupItem>
+                        </ListGroup>
+                        <div id="button-box">
+                            <ButtonToolbar>
+                                <Button bsStyle="success">Download</Button>
+                                <Button bsStyle="primary">Read online</Button>
+                            </ButtonToolbar>
+                        </div>
+                    </div>
+                    <div className="clear"></div>
+                    <div id="item-content-box">
+                        <ListGroup>
+                            <ListGroupItem header="Content">{item.content}</ListGroupItem>
+                        </ListGroup>
+                    </div>
+                </div>
+                <div className="clear"></div>
+            </div>
+            <div><Footer /></div>
+        </div>
+    );
+};
+
+const item = {
+    name: 'Book1',
+    category: 'Category1',
+    author: 'Author1',
+    year: 'Year1',
+    genre: 'Genre1',
+    lang: 'Language1',
+    image: image,
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget egestas metus. Nullam blandit leo lorem, vel euismod ligula hendrerit nec. Curabitur rhoncus quam urna, quis fringilla lacus dapibus blandit. Duis facilisis massa id urna convallis, a pulvinar justo pulvinar. Phasellus rutrum finibus enim, nec laoreet augue commodo quis.',
+};
+
+export { Info };
